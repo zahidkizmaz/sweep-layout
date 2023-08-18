4e27d43ef9217aadfd8d1a9a4945de3ae7d8646d
Zahid Kizmaz <tech@zahid.rocks>
2023-08-18 10:15:43 +0200

    fix scrolling

--- src/keymap.c
+++ src/keymap.c
@@ -77,7 +77,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         //+------------------------------------------------------------------------+       +------------------------------------------------------------------------+
         /**/ KC_NO       , KC_NO       , KC_NO       , KC_NO       , QK_BOOT     ,/*       */ QK_BOOT     , KC_NO       , KC_NO       , KC_NO       , KC_NO     ,/**/
         /**/ KC_NO       , KC_NO       , KC_NO       , KC_BTN1     , KC_BTN2     ,/*       */ KC_MS_L     , KC_MS_D     , KC_MS_U     , KC_MS_R     , KC_NO     ,/**/
-        /**/ KC_NO       , KC_NO       , KC_NO       , KC_NO       , KC_NO       ,/*       */ KC_WH_L     , KC_WH_D     , KC_WH_U     , KC_WH_R     , KC_NO     ,/**/
+        /**/ KC_NO       , KC_NO       , KC_NO       , KC_NO       , KC_NO       ,/*       */ KC_WH_L     , KC_WH_U     , KC_WH_D     , KC_WH_R     , KC_NO     ,/**/
         /*------------------------------------------*/ TD(L_TAP)   , KC_NO       ,/*       */ KC_NO       , TD(L_TAP)     /*---------------------------------------*/
                                                     /*----------------------------*/       /*----------------------------*/
     )
