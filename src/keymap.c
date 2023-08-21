b828e7a9c0b09a48b5f4a24a2eb71b845627abab
Zahid Kizmaz <tech@zahid.rocks>
2023-08-21 22:14:01 +0200

    add numbers to sym layer

--- src/keymap.c
+++ src/keymap.c
@@ -66,7 +66,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [_SYM] = LAYOUT(
         //+------------------------------------------------------------------------+       +-----------------------------------------------------------------------------------+
-        /**/ KC_F1       , KC_F2       , KC_F3       , KC_F4            , KC_F5  ,/*       */ KC_F6      , KC_F7               , KC_F8             , KC_F9      , KC_F10   ,/**/
+        /**/ KC_1        , KC_2        , KC_3        , KC_4             , KC_5   ,/*       */ KC_6       , KC_7                , KC_8              , KC_9       , KC_0     ,/**/
         /**/ KC_ESC      , KC_TAB      , KC_GRV      , KC_DQUO          , KC_CAPS,/*       */ KC_UNDS    , KC_LPRN             , KC_RPRN           , KC_EQUAL   , KC_QUOTE ,/**/
         /**/ KC_LSFT     , KC_NO       , KC_LCTL     , KC_LGUI          , KC_LALT,/*       */ KC_MINUS   , KC_LEFT_BRACKET     , KC_RIGHT_BRACKET  , KC_BSLS    , KC_RSFT  ,/**/
         /*------------------------------------------*/ TD(L_TAP)        , KC_SPC ,/*       */ KC_ENT     , KC_BSPC         /*-------------------------------------------------*/
